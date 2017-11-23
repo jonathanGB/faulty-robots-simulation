9abfc993b5cb61713b209d6ab58a054543226223
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2017-11-23 15:01:32 -0500

    remove startup & executables

--- src/startup.bash
+++
@@ -1,6 +0,0 @@
-case "$OSTYPE" in
-  darwin*)  echo "OSX" && bin/mac_server ;; 
-  linux*)   echo "LINUX" && bin/linux_server ;;
-  msys*)    echo "WINDOWS" && bin/windows_server.exe ;;
-  *)        echo "unknown: $OSTYPE" ;;
-esac
